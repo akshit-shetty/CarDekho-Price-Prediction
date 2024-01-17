0f8519a386812be8985fe6ec1b4a65f032efcf3c
akshit-shetty <102954215+akshit-shetty@users.noreply.github.com>
2024-01-17 20:37:48 +0530

    Update Car Price Prediction using Linear Regression.py

--- Car Price Prediction using Linear Regression - Assignment.py
+++ Car Price Prediction using Linear Regression - Assignment.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## Problem Statement:
+ ## Problem Statement:
 # The CarDekho company maintains a database of the cars sold through their platform. The data represents the cars sold by Car Dekho and the car related features. We have to build a linear regression model to predict the Selling price of the car. Calculate all the error metrics and diagnostic plots to check the regression result. The dataset contains data for around 301 cars sold by CarDekho.
-
-# In[18]:
 
 
 import numpy as np
@@ -18,13 +13,8 @@ from scipy import stats
 
 # ## Read the cars dataset
 
-# In[19]:
-
 
 car = pd.read_csv("car data.csv")
-
-
-# In[20]:
 
 
 car.head(10)
@@ -34,7 +24,6 @@ car.head(10)
 
 # Data quality check
 
-# In[21]:
 
 
 print(car.info())
@@ -43,16 +32,10 @@ print(car.describe())
 
 # Check for missing values
 
-# In[22]:
-
-
 print(car.isnull().sum())
 
 
 # Correlation matrix to identify linear relationships
-
-# In[23]:
-
 
 correlation_matrix = car.corr()
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
@@ -62,14 +45,7 @@ plt.show()
 
 # ## Transform the categorical data.
 
-# In[24]:
-
-
 car_final = car.drop(['Selling_Price'], axis =1)
-
-
-# In[27]:
-
 
 car_final = pd.get_dummies(car_final)
 car_final.shape
@@ -77,8 +53,6 @@ car_final.head()
 
 
 # ## Multiple Linear Regression model
-
-# In[28]:
 
 
 X = car_final
@@ -94,56 +68,10 @@ print(res.summary())
 
 # ## Feature Importance
 
-# In[17]:
-
 
 feature_importance = pd.Series(res.params)
 feature_importance.plot(kind='barh')
 plt.title('Feature Importance (Statsmodels)')
 plt.xlabel('Coefficients')
 plt.show()
-
-
-# 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 

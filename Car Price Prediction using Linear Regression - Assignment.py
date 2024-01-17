b5eb1c66b4a9442ca1b585e023b191518032d160
akshit-shetty <102954215+akshit-shetty@users.noreply.github.com>
2024-01-17 20:20:08 +0530

    Add files via upload

---
+++ Car Price Prediction using Linear Regression - Assignment.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Problem Statement:
+# The CarDekho company maintains a database of the cars sold through their platform. The data represents the cars sold by Car Dekho and the car related features. We have to build a linear regression model to predict the Selling price of the car. Calculate all the error metrics and diagnostic plots to check the regression result. The dataset contains data for around 301 cars sold by CarDekho.
+
+# In[10]:
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from scipy import stats
+
+
+# ## Read the cars dataset
+
+# In[4]:
+
+
+car = pd.read_csv("car data.csv")
+
+
+# In[5]:
+
+
+car.head(10)
+
+
+# ## Exploratory Data Analysis (EDA) 
+
+# Data quality check
+
+# In[6]:
+
+
+print(car.info())
+print(car.describe())
+
+
+# Check for missing values
+
+# In[7]:
+
+
+print(car.isnull().sum())
+
+
+# Correlation matrix to identify linear relationships
+
+# In[11]:
+
+
+correlation_matrix = car.corr()
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('Correlation Matrix')
+plt.show()
+
+
+# ## Transform the categorical data.
+
+# In[12]:
+
+
+car_final = car.drop(['Car_Name','Selling_Price'], axis =1)
+
+
+# In[13]:
+
+
+car_final = pd.get_dummies(car_final)
+car_final.shape
+car_final.head()
+
+
+# ## Multiple Linear Regression model
+
+# In[16]:
+
+
+X = car_final
+X = stats.zscore(X)
+X = sm.add_constant(X)
+y = car['Selling_Price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+res = sm.OLS(y_train, X_train).fit()
+print(res.summary())
+
+
+# ## Feature Importance
+
+# In[17]:
+
+
+feature_importance = pd.Series(res.params)
+feature_importance.plot(kind='barh')
+plt.title('Feature Importance (Statsmodels)')
+plt.xlabel('Coefficients')
+plt.show()
+
+
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
